580d06ebfaa5521a09022d0f40934c22bed1aa8c
WindyBindy <134945382+WindyBindy@users.noreply.github.com>
2026-06-28 10:25:33 +0200

    loader, modal

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import { Component } from "react";
-
+import style from "./Button.module.css"
 class Button extends Component {
     render(){
         const {onClick} = this.props
         return(
-            <button type="button" onClick={onClick} >Завантажити ще</button>
+            <button type="button" onClick={onClick} className={style.button} >Завантажити ще</button>
         )
     }
 }
